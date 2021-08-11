1150190efeff7719ed522eaf288363c5b4c96bf7
Sappurit <80998766+Sappurit@users.noreply.github.com>
2021-08-11 17:05:52 +0700

    update

--- SAP/Sappurit/#BUILD.cmd
+++ SAP/Sappurit/#BUILD.cmd
@@ -1,6 +1,6 @@
 
 
-set version=v16.14.34
+set version=v16.29.39
 
 
 set home=D:\pic\FIFA Mobile\git\YouTube-Vanced-SAP
@@ -19,22 +19,43 @@ zip -9 -j  "%file3%.zip"  "%dir%\Language\split_config.en.apk"  "%dir%\Language\
 zip -9 -j  "%file4%.zip"  "%dir%\Language\split_config.en.apk"  "%dir%\Language\split_config.th.apk"  "%dir%\Arch\split_config.armeabi_v7a.apk"  "%dir%\Theme\dark.apk"
 
 
-start "" "%home%\Split.APKs.Packer\sap.exe"
 echo off
 echo.
 echo.
 echo ----------------------------------------------------------
-echo Do not close this windows!!!
-echo Build all your project(s) with SAP. 
-echo When finished press enter to automatic rename file(s).
+echo DO NOT close these windows !!!
+echo Build all your projects with SAP one by one.
+echo Close SAP to continue the next loop.
+echo When finished, it will automatically rename all APK files.
 echo ----------------------------------------------------------
-echo.
+
+
+FOR /L %%G IN (1,1,4) DO (
+	echo.
+	echo Loop : %%G of 4
+	start /wait "" "%home%\Split.APKs.Packer\sap.exe"
+)
+
+
 pause
 
 
-if exist "%file1%_SAP_Project\build\YouTube_*_SAP.apk"  move /y  "%file1%_SAP_Project\build\YouTube_*_SAP.apk"  "%file1%.apk"
-if exist "%file2%_SAP_Project\build\YouTube_*_SAP.apk"  move /y  "%file2%_SAP_Project\build\YouTube_*_SAP.apk"  "%file2%.apk"
-if exist "%file3%_SAP_Project\build\YouTube_*_SAP.apk"  move /y  "%file3%_SAP_Project\build\YouTube_*_SAP.apk"  "%file3%.apk"
-if exist "%file4%_SAP_Project\build\YouTube_*_SAP.apk"  move /y  "%file4%_SAP_Project\build\YouTube_*_SAP.apk"  "%file4%.apk"
+if exist "%file1%_SAP_Project\build\YouTube_*_SAP.apk"  ( move /y  "%file1%_SAP_Project\build\YouTube_*_SAP.apk"  "%file1%.apk" )
+if exist "%file2%_SAP_Project\build\YouTube_*_SAP.apk"  ( move /y  "%file2%_SAP_Project\build\YouTube_*_SAP.apk"  "%file2%.apk" )
+if exist "%file3%_SAP_Project\build\YouTube_*_SAP.apk"  ( move /y  "%file3%_SAP_Project\build\YouTube_*_SAP.apk"  "%file3%.apk" )
+if exist "%file4%_SAP_Project\build\YouTube_*_SAP.apk"  ( move /y  "%file4%_SAP_Project\build\YouTube_*_SAP.apk"  "%file4%.apk" )
+
+if exist "%file1%_SAP_Project"                          ( rd  /q /s  "%file1%_SAP_Project" )
+if exist "%file2%_SAP_Project"                          ( rd  /q /s  "%file2%_SAP_Project" )
+if exist "%file3%_SAP_Project"                          ( rd  /q /s  "%file3%_SAP_Project" )
+if exist "%file4%_SAP_Project"                          ( rd  /q /s  "%file4%_SAP_Project" )
+
+if exist "%file1%.zip"                                  ( del /q /f  "%file1%.zip"         )
+if exist "%file2%.zip"                                  ( del /q /f  "%file2%.zip"         )
+if exist "%file3%.zip"                                  ( del /q /f  "%file3%.zip"         )
+if exist "%file4%.zip"                                  ( del /q /f  "%file4%.zip"         )
+
+
 pause
+
 
